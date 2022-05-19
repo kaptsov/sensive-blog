050b6a50c515dd5e539f1b682fdad73630c48e39
kaptsov <kaptsovaa@gmail.com>
2022-05-19 22:29:01 +0400

    Избавился от Сортировки и фукции get_related_posts

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,6 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -37,9 +33,8 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')
+    most_popular_tags = list(popular_tags)[:5]
 
     context = {
         'most_popular_posts': [
@@ -78,9 +73,8 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')
+    most_popular_tags = list(popular_tags)[:5]
 
     most_popular_posts = []
 
@@ -97,11 +91,11 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')
+    most_popular_tags = list(popular_tags)[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(Count('likes')).order_by('likes__count')
+    most_popular_posts = list(most_popular_posts)[-5:]
 
     related_posts = tag.posts.all()[:20]
 
