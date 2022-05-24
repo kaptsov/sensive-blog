62ec9a8d3612acb3ab7bff4a75bc06e8c4c900ee
kaptsov <kaptsovaa@gmail.com>
2022-05-25 01:05:06 +0400

    Сделан легкий рефакторинг

--- blog/views.py
+++ blog/views.py
@@ -25,18 +25,22 @@ def serialize_tag(tag):
     }
 
 
+def get_tag_prefetch():
+    return Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts')))
+
+
 def index(request):
 
     most_popular_posts = Post.objects\
         .popular()\
         .prefetch_related('author')\
-        .prefetch_related(Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts'))))[:5]\
+        .prefetch_related(get_tag_prefetch())[:5]\
         .fetch_with_comments_count()
 
     most_fresh_posts = Post.objects\
         .fresh()\
         .prefetch_related('author') \
-        .prefetch_related(Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts'))))[:5] \
+        .prefetch_related(get_tag_prefetch())[:5] \
         .fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
@@ -61,7 +65,7 @@ def post_detail(request, slug):
     most_popular_posts = Post.objects\
         .popular()\
         .prefetch_related('author')\
-        .prefetch_related(Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts'))))[:5]\
+        .prefetch_related(get_tag_prefetch())[:5]\
         .fetch_with_comments_count()
 
     tags = Tag.objects.popular()
@@ -103,12 +107,12 @@ def tag_filter(request, tag_title):
     most_popular_posts = Post.objects\
         .popular()\
         .prefetch_related('author')\
-        .prefetch_related(Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts'))))[:5]\
+        .prefetch_related(get_tag_prefetch())[:5]\
         .fetch_with_comments_count()
 
     related_posts = tag.posts\
         .prefetch_related('author')\
-        .prefetch_related(Prefetch('tags', Tag.objects.annotate(num_posts=Count('posts'))))[:20]\
+        .prefetch_related(get_tag_prefetch())[:20]\
         .fetch_with_comments_count()
 
     context = {
